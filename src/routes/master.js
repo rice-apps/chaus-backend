f5022a22c97d68156edb8b31f17c83d4ba737c5a
Josh-Dunning <Josh.Dunning@rice.edu>
2018-02-17 15:52:40 -0600

    New backend calls

--- src/routes/master.js
+++ src/routes/master.js
@@ -1,5 +1,11 @@
 const schedule = require('../models/scheduleModel').schedule;
 
+/*
+app.get('/master/schedule', getSchedule)
+
+Input: None
+Output: Full schedule
+*/
 const getSchedule = (req, res) => {
     schedule.find({}).exec((err, r) => {
         if (err) {
@@ -10,85 +16,67 @@ const getSchedule = (req, res) => {
     })
 }
 
-const getDay = (req, res) => {
-    schedule.find({}, {_id: 0, week: {$elemMatch: {day: req.params.day}}}).exec((err, r) => {
-        if (err) {
-            res.send('error has occurred')
-        } else {
-            res.send(r[0])
-        }
-    })
-}
+/*
+app.get('/master/shift/:weekday?/:hour?', getShift)
 
+Input: day and hour ints
+Output: shift object showing:
+-shift status (boolean open or closed)
+-shift hour (int [0-17])
+-lists of users with availabilities 1, 2, 3, and 4
+*/
 const getShift = (req, res) => {
-    schedule.find({}).exec((err, r) => {
-        if (err) {
-            res.send('error has occurred')
-        } else {
-            res.send(
-                r[0].week.filter(day => day.day == req.params.weekday)[0].shifts.filter(shift => shift.hour == req.params.hour)
-            )
+  schedule.find({}).lean().exec((err, allShifts) => {
+    if (err) {
+        res.send('error has occurred')
+    } 
+    else {
+        requestedShift = allShifts[0].week[parseInt((req.params.weekday * 18) + req.params.hour)];
+        if (!requestedShift.status) {
+            var newShift = {"status": false}
         }
-    })
+        else {
+            var newShift = {"status": true, "hour": req.params.hour, 1: [], 2: [], 3: [], 4: []}
+            newShift.scheduled = requestedShift.scheduled
+            Object.keys(requestedShift).map((key, index) => {
+                if (!(key == "scheduled" || key == "status")) {
+                    newShift[requestedShift[key]].push(key);
+                }
+            });
+        }
+        res.send(newShift);
+    }
+  })
 }
 
-const getAvailable = (req, res) => {
-    schedule.find({}).exec((err, r) => {
-        if (err) {
-            res.send('error has occurred')
-        } else {
-            res.send(
-                r[0].week.map(
-                    day => {
-                        filteredShifts = day.shifts.map(
-                            shift => {
-                                var newShift = {};
-                                if (shift.availability.indexOf(req.params.netid) != -1) {
-                                    newShift['available'] = true
-                                } else {
-                                    newShift['available'] = false
-                                }
-                                newShift['hour'] = shift.hour
-                                newShift['changed'] = false
-                                newShift['status'] = true
-                                return newShift
-                            })
-                        var res = {};
-                        res[day.day] = filteredShifts
-                        return res;
-                    }))
-        }
-    })
+/*
+app.get('/master/hourtotal/:netid?', getHourTotal)
+
+Input: user netid
+Output: total number of hours given employee is currently scheduled
+
+NOT FINISHED DOESN'T WORK
+*/
+const getHourTotal = (req, res) => {
+  schedule.find({}).lean().exec((err, allShifts) => {
+    if (err) {
+        res.send('error has occurred')
+    } 
+    else {
+        hoursum = 0
+        allShifts[0].week.map(shift => {
+            console.log(req.params.netid);
+            console.log(shift.scheduled[1]);
+            console.log(req.params.netid.toString() in shift.scheduled);
+            /*if (shift.scheduled.includes(req.params.netid)) {
+                hoursum++;
+            }*/
+        })
+        res.send(hoursum.toString());
+    }
+  })
 }
 
-const getScheduled = (req, res) => {
-    schedule.find({}).exec((err, r) => {
-        if (err) {
-            res.send('error has occurred')
-        } else {
-            res.send(
-                r[0].week.map(
-                    day => {
-                        filteredShifts = day.shifts.map(
-                            shift => {
-                                var newShift = {};
-                                if (shift.schedule.indexOf(req.params.netid) != -1) {
-                                    newShift['schedule'] = true
-                                } else {
-                                    newShift['schedule'] = false
-                                }
-                                newShift['hour'] = shift.hour
-                                newShift['changed'] = false
-                                newShift['status'] = true
-                                return newShift
-                            })
-                        var res = {};
-                        res[day.day] = filteredShifts
-                        return res;
-                    }))
-        }
-    })
-}
 
 function updateShift(shifts, changedshift, netId) {
     var availableOriginal = shifts[changedshift.hour - 7].availability
@@ -149,10 +137,8 @@ const putSchedule = (req, res) => {
 
 module.exports = app => {
     app.get('/master/schedule', getSchedule)
-    app.get('/master/:day?', getDay)
-    app.get('/master/available/:netid?', getAvailable)
-    app.get('/master/schedule/:netid?', getScheduled)
     app.get('/master/shift/:weekday?/:hour?', getShift)
+    app.get('/master/hourtotal/:netid?', getHourTotal)
     app.put('/master/update/availability/:netid?', putAvailability)
     app.put('/master/update/:weekday?/:hour?', putSchedule)
 }
