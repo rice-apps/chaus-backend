bf57c9acb994745c3f60d0c4dbc04c7584300b59
Josh-Dunning <Josh.Dunning@rice.edu>
2018-02-17 13:55:40 -0600

    Calls to get shift and availability

--- src/routes/schedule.js
+++ src/routes/schedule.js
@@ -27,12 +27,38 @@ const getDay = (req, res) => {
 
 // Helper Function for getDay
 const parseShifts = (shifts, day) => {
-  let requestedShifts = []
+  var requestedShifts = []
   // Gets 18 shifts
-  for (let i = (day * 18); i < i + 18; i++) {
+  for (var i = (day * 18); i < i + 18; i++) {
     requestedShifts.push(shifts[i])
   }
   return requestedShifts
+}
+
+const getShift = (req, res) => {
+  // Get a specific shift
+  schedule.find({}).lean().exec((err, allShifts) => {
+    if (err) {
+        res.send('error has occurred')
+    } 
+    else {
+        requestedShift = allShifts[(req.params.day * 18) + req.params.hour]
+        res.send(requestedShift)
+    }
+  })
+}
+
+const getAvailability = (req, res) => {
+  // Get the shifts of a certain user with a specified availability type
+  schedule.find({}).lean().exec((err, allShifts) => {
+    if (err) {
+        res.send('error has occurred')
+    } 
+    else {
+        filtered_shifts = r[0].week.filter(shift => shift[req.params.netid] = req.params.availability)
+        res.send(filtered_shifts)
+    }
+  })
 }
 
 const changeShift = (req, res) => {
@@ -45,6 +71,8 @@ const changeShift = (req, res) => {
 module.exports = app => {
     app.get('/schedule', getSchedule)
     app.get('/schedule/:day?', getDay)
+    app.get('/schedule/:day?/:hour?', getShift)
+    app.get('/schedule/:availability?/:netid?', getAvailability)
     app.put('/schedule/shift/:day?/:hour?', changeShift)
 }
 
