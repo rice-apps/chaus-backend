a29c43451ef9d9afe2f6c4d09af97cb4a653f53f
ThunderLight25 <cogsteeler2@gmail.com>
2018-03-02 22:51:48 -0600

    Added remove user from scheduled method

--- src/routes/schedule.js
+++ src/routes/schedule.js
@@ -47,50 +47,55 @@ const addUserToShift = (req, res) => {
     let original_shifts = shifts[0].week
     shift = findShift(shifts[0], day, hour)
     newScheduled = shift.scheduled
-    newScheduled.push(user)
-    shift.scheduled = newScheduled
-    let id = shift._id
-    schedule.replaceOne({}, {week: shifts[0].week}).exec((err, doc) => {
-      if (err) {
-        console.log("Rip")
-      }
-      console.log("yay")
-      res.send("Success")
-    })
-    // schedule.findOne({"_id": shift._id}, (err, shift) => {
-    //   if (err) {
-    //     return err
-    //   }
-    //   console.log(shift)
-    //   shift.save()
-    // })
-    // Trying elemMatch (elmeme)
-    // schedule.find({week: {$elemMatch: {_id: id}}}, (err, doc) => {
-    //   if (err) {
-    //     console.log(err)
-    //     console.log("Oops! An error occurred.")
-    //   }
-    //   res.send(doc)
-    // })
-    //
-    // shifts[0].week.findOneAndUpdate({_id: id}, {$set:{scheduled: newScheduled}}, (err, doc) => {
-    //   if (err) {
-    //     console.log("Oops! Something went wrong.")
-    //   }
-    //   console.log(doc)
-    //   res.send(doc)
-    // })
-    // let shiftIndex = shifts[0].week.indexOf(shift)
-    // shifts[0].week[shiftIndex].markModified('scheduled')
-    // shifts[0].week[shiftIndex].save((err) => {
-    //   if (err) {
-    //     console.log(err)
-    //   }
-    //   console.log("saved!")
-    // })
-    // res.send(shift)
+    if (newScheduled.indexOf(user) >= 0) {
+      res.send("User Already Scheduled")
+    }
+    else {
+      // Add user to scheduled array 
+      newScheduled.push(user)
+      shift.scheduled = newScheduled
+      // Replaces entire week schedule to accomodate for changes
+      schedule.replaceOne({}, {week: shifts[0].week}).exec((err, doc) => {
+        if (err) {
+          console.log("Rip")
+        }
+        res.send("Success")
+      })
+    }
   })
-  // Unfinished
+}
+
+const removeUserFromShift = (req, res) => {
+  // Schedules user for shift
+  const day = req.params.day
+  const hour = req.params.hour
+  const user = req.body.user
+  schedule.find({}, (err, shifts) => {
+    if (err) {
+      res.send(err)
+    }
+    let original_shifts = shifts[0].week
+    shift = findShift(shifts[0], day, hour)
+    newScheduled = shift.scheduled
+    if (newScheduled.indexOf(user) < 0) {
+      res.send("User Not Scheduled")
+    }
+    else {
+      // sequence of steps to remove user
+      // get user index
+      user_index = newScheduled.indexOf(user)
+      // splice array, returns new array
+      newScheduled.splice(user_index, 1)
+      shift.scheduled = newScheduled
+      // Replaces entire week schedule to accomodate for changes
+      schedule.replaceOne({}, {week: shifts[0].week}).exec((err, doc) => {
+        if (err) {
+          console.log("Rip")
+        }
+        res.send("Success")
+      })
+    }
+  })
 }
 
 const findShift = (shifts, day, hour) => {
@@ -104,4 +109,5 @@ module.exports = app => {
     app.get('/schedule', getSchedule)
     app.get('/schedule/:day?', getDay)
     app.put('/schedule/shift/:day?/:hour?', addUserToShift)
+    app.delete('/schedule/shift/:day?/:hour?', removeUserFromShift)
 }
