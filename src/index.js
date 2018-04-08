02ed819bd78f654f3234261e2803125aa6ebd3f3
ThunderLight25 <cogsteeler2@gmail.com>
2018-04-08 16:05:11 -0500

    Auth implemented using CAS

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ require('./routes/users')(app)
 require('./routes/master')(app)
 require('./routes/employee')(app)
 require('./routes/schedule')(app)
+require('./routes/auth')(app)
 
 // Get the port from the environment, i.e., Heroku sets it
 const port = process.env.PORT || 3000
