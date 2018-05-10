7ccafeb01414c5923bedd859ba232d3b1b0d4a86
Jeffrey Wang <jeffreywangch@gmail.com>
2018-05-10 17:45:09 -0500

    added config to the backend setup, need to manually create an .env file locally

--- src/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    secret: 'TEST_SECRET',
-    db_uri: 'mongodb://jhw5:jhw5@ds143449.mlab.com:43449/chaus',
-    CASValidateURL: 'https://idp.rice.edu/idp/profile/cas/serviceValidate',
-    thisServiceURL: 'http://localhost:8080/auth',
-    frontendURL: 'http://localhost:8080'
-};
